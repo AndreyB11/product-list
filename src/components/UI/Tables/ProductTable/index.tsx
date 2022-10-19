7ead56c7eb96ebec25b8355180027a7821d1d403
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-10-19 13:05:13 +0300

    table initial

---
+++ src/components/UI/Tables/ProductTable/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { IProduct } from "shared/models";
+
+interface IProps {
+  products: IProduct[];
+}
+
+export const ProductTable = ({ products }: products) => {
+  return <div>index</div>;
+};
