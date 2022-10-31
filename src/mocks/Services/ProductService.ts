3f0153fbfb1aa024f3f82ea2f7d007570e33fad3
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-10-31 13:10:10 +0200

    add random images

--- src/mocks/Services/ProductService.ts
+++ src/mocks/Services/ProductService.ts
@@ -2,6 +2,16 @@ import { IProduct } from "shared/models";
 
 class ProductService {
   private products: IProduct[] = [];
+  private readonly images: string[] = [
+    "https://cdn.pixabay.com/photo/2020/04/03/19/02/virus-4999857__340.png",
+    "https://cdn.pixabay.com/photo/2017/07/15/21/46/sunflower-2507845__340.png",
+    "https://cdn.pixabay.com/photo/2016/11/15/21/29/illustration-1827583__340.jpg",
+    "https://cdn.pixabay.com/photo/2020/04/30/17/25/monkey-soldier-5113779__340.png",
+  ];
+
+  private getRandomImage(): string {
+    return this.images[Math.floor(Math.random() * (this.images.length - 1))];
+  }
 
   async getAllProducts() {
     return this.products;
@@ -10,6 +20,7 @@ class ProductService {
   async addProduct(product: Omit<IProduct, "id">) {
     const newProduct: IProduct = {
       ...product,
+      image: this.getRandomImage(),
       id: String(Math.floor(Math.random() * 100)),
     };
 
