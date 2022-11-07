87e122a5f0b9d88fe45944ef0f216a8941c27b6b
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-07 11:35:18 +0200

    fix delete modal styles

--- src/components/UI/Modals/DeleteModal/index.tsx
+++ src/components/UI/Modals/DeleteModal/index.tsx
@@ -1,13 +1,42 @@
 import { Button, Grid, IconButton, Typography } from "@mui/material";
-import { GenericModal } from "../GenericModal";
 import CloseIcon from "@mui/icons-material/Close";
-import { deleteModalStyles } from "./deleteModalStyles";
+import { GenericModal } from "../GenericModal";
+import { Style } from "shared/theme";
 import { globalStyles } from "shared/theme";
 
 interface IProps {
   visible: boolean;
   onCancel: () => void;
 }
+
+const deleteModalStyles: Style = {
+  modal: {
+    position: "absolute",
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "white",
+    width: "300px",
+    height: "200px",
+    boxShadow: 24,
+    outline: "none",
+  },
+  innerContainer: {
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+  },
+  heading: {
+    backgroundColor: "#191a19",
+    py: 2,
+    mb: 2,
+  },
+  icon: {
+    position: "absolute",
+    right: 4,
+    top: 5,
+  },
+};
 
 export const DeleteModal = ({ visible, onCancel }: IProps) => (
   <GenericModal
