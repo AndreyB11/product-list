51cdd46b1676958ddeb23cad98927ad9443d5000
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-07 15:12:18 +0200

    change actions and update service

--- src/shared/services/ProductService/index.ts
+++ src/shared/services/ProductService/index.ts
@@ -1,8 +1,8 @@
 import { IProduct } from "shared/models";
 
 export class ProductService {
-  static async getAllProducts() {
-    return [] as IProduct[];
+  static async getAllProducts(): Promise<IProduct[]> {
+    return [];
   }
 
   static async addProduct(product: IProduct) {}
