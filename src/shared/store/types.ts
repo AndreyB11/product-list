c53c6d53e8fc3b307b0c92302c0733de9c30ec43
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-07 16:26:42 +0200

    remove unused

--- src/shared/store/types.ts
+++ src/shared/store/types.ts
@@ -1,7 +1,2 @@
 export type ActionTypes<T extends Record<string, (...args: any) => any>> =
   ReturnType<T[keyof T]>;
-
-export interface Action<T, P> {
-  readonly type: T;
-  readonly payload?: P;
-}
