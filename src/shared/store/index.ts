2730a7a55cc4733a8de9d882e55b6c4f34156738
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-07 16:53:22 +0200

    add product hook

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import { productReducer } from "./reducers/products";
 import createSagaMiddleware from "@redux-saga/core";
 import { rootSaga } from "./saga";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -9,8 +10,12 @@ const rootReducer = combineReducers({
   productReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
-
 export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 sagaMiddleware.run(rootSaga);
