5188903c7672ca1e1e2fe855fc99bda7f8a5f964
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-08 12:33:08 +0200

    add mui theme

--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -1,7 +1,15 @@
-import { SxProps } from "@mui/material";
+import { createTheme, SxProps } from "@mui/material";
 import { Theme } from "@mui/system";
 
 export type Style = Record<string, SxProps<Theme>>;
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f0f0f0",
+    },
+  },
+});
 
 export const globalStyles: Style = {
   whiteColor: {
