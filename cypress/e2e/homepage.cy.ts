acc68493a0d95bcca785ca4c1306b4df6af69449
Andrey Burtsev <andrey.burtsev@appsforce.io>
2022-11-10 10:43:01 +0200

    add testing-library to cypress

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -2,44 +2,44 @@ describe("homepage spec", () => {
   it("adds product", () => {
     cy.visit("/");
 
-    cy.get('[data-testid="add-product-button"]').click();
-    cy.get('[data-testid="product-name-field"]').type("test");
-    cy.get('[data-testid="product-price-field"]').type("123");
-    cy.get('[data-testid="product-brand-field"]').type("Nike");
-    cy.get('[data-testid="product-submit-button"]').click();
+    cy.findByTestId("add-product-button").click();
+    cy.findByTestId("product-name-field").type("test");
+    cy.findByTestId("product-price-field").type("123");
+    cy.findByTestId("product-brand-field").type("Nike");
+    cy.findByTestId("product-submit-button").click();
   });
 
   it("edits product", () => {
     cy.visit("/");
 
-    cy.get('[data-testid="add-product-button"]').click();
-    cy.get('[data-testid="product-name-field"]').type("test");
-    cy.get('[data-testid="product-price-field"]').type("123");
-    cy.get('[data-testid="product-brand-field"]').type("Nike");
-    cy.get('[data-testid="product-submit-button"]').click();
+    cy.findByTestId("add-product-button").click();
+    cy.findByTestId("product-name-field").type("test");
+    cy.findByTestId("product-price-field").type("123");
+    cy.findByTestId("product-brand-field").type("Nike");
+    cy.findByTestId("product-submit-button").click();
 
     const editBtn = cy
-      .get('[data-testid="table"]')
+      .findByTestId("table")
       .last()
-      .find('[data-testid="edit-product-button"]');
+      .findByTestId("edit-product-button");
 
     editBtn.click();
-    cy.get('[data-testid="product-name-field"]').type("test2");
-    cy.get('[data-testid="product-price-field"]').type("1234");
-    cy.get('[data-testid="product-brand-field"]').type("Puma");
-    cy.get('[data-testid="product-submit-button"]').click();
+    cy.findByTestId("product-name-field").type("test2");
+    cy.findByTestId("product-price-field").type("1234");
+    cy.findByTestId("product-brand-field").type("Puma");
+    cy.findByTestId("product-submit-button").click();
   });
 
   it("deletes product", () => {
     cy.visit("/");
 
-    cy.get('[data-testid="add-product-button"]').click();
-    cy.get('[data-testid="product-name-field"]').type("test");
-    cy.get('[data-testid="product-price-field"]').type("123");
-    cy.get('[data-testid="product-brand-field"]').type("Nike");
-    cy.get('[data-testid="product-submit-button"]').click();
+    cy.findByTestId("add-product-button").click();
+    cy.findByTestId("product-name-field").type("test");
+    cy.findByTestId("product-price-field").type("123");
+    cy.findByTestId("product-brand-field").type("Nike");
+    cy.findByTestId("product-submit-button").click();
 
-    cy.get('[data-testid="delete-product-button"]').click();
-    cy.get('[data-testid="confirm-delete-button"]').click();
+    cy.findByTestId("delete-product-button").click();
+    cy.findByTestId("confirm-delete-button").click();
   });
 });
