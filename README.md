248ced8fa22d1b9e3290673514c4282c2c265cc7
AndreyB11 <Itay@interai.co>
2023-03-04 16:13:26 +0200

    connect to prod api

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Onboarding task appsforce
 
+## You can preview [Here](https://product-list-flask-rest-api.onrender.com)
+
 ### How to run locally
 
 - `yarn i`
--- src/shared/services/API/index.ts
+++ src/shared/services/API/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://flask-rest-api-2cgk.onrender.com";
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: API_URL,
